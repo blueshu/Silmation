e0a46bc68ab5b2e6f31a04edf3da754f9d824c3a
Kevin <jsgu@aceinna.com>
2018-07-25 13:28:47 +0800

    update error in update and delete

--- gnss_ins_sim/sim/sim_data_upload.py
+++ gnss_ins_sim/sim/sim_data_upload.py
@@ -40,7 +40,6 @@ class DataUpload(object):
     def begin_update_files(self):
         if self.totalFiles > 0:
             self.update_files()
-
     def processCall(self,process,total):
         if process == total:
             if self.index < self.totalFiles-1:
@@ -59,9 +58,16 @@ class DataUpload(object):
             append_blob_service = AppendBlobService(account_name='navview', account_key='+roYuNmQbtLvq2Tn227ELmb6s1hzavh0qVQwhLORkUpM0DN7gxFc4j+DF/rEla1EsTN2goHEA1J92moOM/lfxg==', protocol='http')
             append_blob_service.create_blob(container_name='data', blob_name=name,content_settings=ContentSettings(content_type='text/plain'))
             append_blob_service.append_blob_from_bytes(container_name='data',blob_name=name,blob=text,progress_callback=self.processCall)
+            f.close()
+
         except Exception as e:
             print(e)
 
     def clear_files(self):
-        shutil.rmtree(self.dirPath)
+        filePath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), self.dirPath))
+        shutil.rmtree(filePath)
         print('end')
+
+
+#if __name__ == '__main__':
+    #DataUpload(dirPath='../../demo_saved_data/2018-07-25-13-01-51').begin_update_files()
