24f02530f9730b30ec45c200f450915001809705
Kevin <jsgu@aceinna.com>
2018-08-28 10:48:32 +0800

    test

--- SimulationFunction/gnss_ins_sim/sim/sim_data_upload.py
+++ SimulationFunction/gnss_ins_sim/sim/sim_data_upload.py
@@ -71,7 +71,7 @@ class DataUpload(object):
         print text
         append_blob_service = AppendBlobService(account_name='navview', account_key='+roYuNmQbtLvq2Tn227ELmb6s1hzavh0qVQwhLORkUpM0DN7gxFc4j+DF/rEla1EsTN2goHEA1J92moOM/lfxg==', protocol='http')
         append_blob_service.create_blob(container_name='data', blob_name=name,content_settings=ContentSettings(content_type='text/plain'))
-        append_blob_service.append_blob_from_bytes(container_name='data',blob_name=name,blob=text)    
+        append_blob_service.append_blob_from_text(container_name='data',blob_name=name,text=text)
 
 
 #if __name__ == '__main__':
