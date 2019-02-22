64d08f7dedd2348d50d5d6ada83d60a54eb32f11
blueshu <jsgu@aceinna.com>
2019-02-22 15:09:21 +0800

    mv simulation function to function2 for backup

---
+++ SimulationFunction2/gnss_ins_sim/kml_gen/kml_gen.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+# Fielname = kml_gen.py
+
+"""
+Generate .kml file contents from LLA
+Created on 2017-11-29
+@author: dongxiaoguang
+"""
+
+import os
+import math
+import numpy as np
+from ..geoparams import geoparams
+from ..attitude import attitude
+
+R2D = 180.0/math.pi
+
+def kml_gen(data_dir, pos, template_file='template.kml', name='pathgen', convert_to_lla=False):
+    '''
+    Generate .kml file contents from position data.
+    Args:
+        data_dir: directory to save the .kml files.
+        pos: nx3 [lat, lon, alt in rad and m, or [x, y, z] in m
+        template_file: template kml file. The line 'REPALCE THIS WITH COORDINATES'
+                       will be replaced by coordinates defined in pos.
+        name: string name of this trajectory.
+        convert_to_lla: true if position data are generated in a virtual inertial frame.
+            xyz-form position data will be converted to [Lat Lon Alt] coordinates.
+            See imu_sim and pathgen for details about the virtual inertial frame.
+    Returns:
+        kml_contents: string contents of kml file.
+    '''
+    # get lla from pos
+    pos = pos.copy()
+    n = pos.shape[0]
+    if convert_to_lla is False:
+        lla = pos
+        lla[:, 0] = lla[:, 0] * R2D
+        lla[:, 1] = lla[:, 1] * R2D
+    else:
+        # ned to lla
+        lla = np.zeros(pos.shape)
+        # virtual inertial frame is defined by initial position
+        lla[0, :] = geoparams.xyz2lla(pos[0, :])
+        ecef_to_ned = attitude.rot_y(-math.pi/2.0-lla[0, 0]).dot(attitude.rot_z(lla[0, 1]))
+        for i in range(1, n):
+            ned_pos = pos[i, :]-pos[0, :]
+            ecef_pos = pos[0, :] + ecef_to_ned.T.dot(ned_pos)
+            lla[i, :] = geoparams.xyz2lla(ecef_pos)
+        # rad to deg
+        lla[:, 0] = lla[:, 0] * R2D
+        lla[:, 1] = lla[:, 1] * R2D
+        # save lla to .csv file. ** This is needed by the web version.
+        header_line = 'Latitude (deg), Longitude (deg), Altitude (deg)'
+        file_name = data_dir + '//' + name + '_LLA.csv'
+        np.savetxt(file_name, lla, header=header_line, delimiter=',', comments='')
+    # gen kml according to data and template
+    if template_file is None:
+        template_file = os.path.join(os.path.dirname(__file__), 'template.kml')
+    else:
+        if not os.path.isabs(template_file):
+            template_file = os.path.join(os.path.dirname(__file__), template_file)
+    fp = open(template_file)
+    lines = ''
+    for line in fp:
+        if line == 'REPALCE THIS WITH COORDINATES\n':
+            max_points = 40000.0
+            step = int(math.ceil(n/max_points))
+            for i in range(0, n, step):
+                if i == 0:
+                    lines = lines + '\t\t\t\t'
+                lines = lines + ('%f,%f,%f ' %(lla[i, 1], lla[i, 0], lla[i, 2]))
+            lines = lines + '\n'
+        elif 'PATHGEN' in line:
+            line = line.replace('PATHGEN', name)
+            lines = lines + line
+        else:
+            lines = lines + line
+    # close template file
+    fp.close()
+    # write contents into .kml file
+    kml_file = data_dir + '//' + name + '.kml'
+    fp = open(kml_file, 'w')
+    fp.write(lines)
+    fp.close()
+    
---
+++ SimulationFunction2/gnss_ins_sim/sim/sim_data_upload.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# Fielname = sim_data.py
+
+"""
+update to azure bolb data class.
+Created on 2018-7-25
+@author: Kevin
+"""
+import os,sys
+import shutil
+import datetime
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'myenv/Lib/site-packages')))
+from azure.storage.blob import ContentSettings,AppendBlobService
+
+class DataUpload(object):
+    '''
+    
+    '''
+    def __init__(self, folderName):
+        #upload in az function folder
+        self.folderName = folderName 
+        self.index = 0
+        self.totalFiles = 0
+        self.files = None #for save output files
+
+    def readFils(self):
+        pathName = os.path.abspath(os.path.join(os.path.dirname( __file__ ), self.dirPath))
+        for root, dirs, files in os.walk(pathName):
+            self.files = files
+            self.totalFiles = len(files)
+            for name in files:
+                self.update_files(name)
+
+    def begin_update_files(self,dirPath):
+        self.dirPath = dirPath
+        self.readFils()
+        #if self.totalFiles > 0:
+            #self.update_files()
+
+    def processCall(self,process,total):
+        if process == total:
+            if self.index < self.totalFiles-1:
+                self.index += 1
+            else: 
+                self.clear_files()
+
+    def update_files(self,fileName):
+        try:
+            filePath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), self.dirPath)) +'/'+ fileName
+            f = open(filePath, 'r')
+            text = f.read()
+            f.close()
+            name = self.folderName + '/' + fileName
+            append_blob_service = AppendBlobService(account_name='navview', account_key='+roYuNmQbtLvq2Tn227ELmb6s1hzavh0qVQwhLORkUpM0DN7gxFc4j+DF/rEla1EsTN2goHEA1J92moOM/lfxg==', protocol='http')
+            append_blob_service.create_blob(container_name='data', blob_name=name,content_settings=ContentSettings(content_type='text/plain'))
+            append_blob_service.append_blob_from_bytes(container_name='data',blob_name=name,blob=text,progress_callback=self.processCall)
+        except Exception as e:
+            print(e)
+
+    def clear_files(self):
+        filePath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), self.dirPath))
+        #filePath = os.path.abspath('.//demo')
+        shutil.rmtree(filePath)
+        print('finished')
+
+    def update_status(self,status = 0, errorMsg = None):
+        if self.files is not None:
+            text = str(status) + '&,&' + datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')+'&,&'+'&,&'.join(self.files)
+        else:
+            text = str(status) + '&,&' + datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+        if errorMsg is not None:
+            text = str(status) + '&,&' + datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT') +'&,&'+ errorMsg
+        name = self.folderName + '/status.cvs'
+        append_blob_service = AppendBlobService(account_name='navview', account_key='+roYuNmQbtLvq2Tn227ELmb6s1hzavh0qVQwhLORkUpM0DN7gxFc4j+DF/rEla1EsTN2goHEA1J92moOM/lfxg==', protocol='http')
+        append_blob_service.create_blob(container_name='data', blob_name=name,content_settings=ContentSettings(content_type='text/plain'))
+        append_blob_service.append_blob_from_text(container_name='data',blob_name=name,text=text)
+
+#if __name__ == '__main__':
+    #DataUpload(dirPath='../../demo_saved_data/2018-07-25-13-01-51').begin_update_files()
